57243c0f504ac7a2a3793455f575d79c565c0046
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2017-05-04 10:32:26 -0400

    modificaciones para que solo almacene solo un link sin repetirlo

--- src/lib/crawler/guardar.js
+++ src/lib/crawler/guardar.js
@@ -1,9 +1,20 @@
 function guardar(pModel, pVector){
   console.log("Iniciando el guardado multiple");
   return new Promise((resolve, reject) => {
-    pModel.insert(pVector)
-    .then(r => resolve(r))
-    .catch(e => reject(e));
+    pVector.map(pItem => {
+      pModel.find({url: pItem.url})
+            .toArray()
+            .then(items => {
+              if(items.length ==  0){
+                pModel.insert([pItem])
+                      .then(r => resolve(r))
+                      .catch(e => reject(e));
+              }
+              // else{
+              //   console.log('Ya existe la url dentro dela BD.');
+              // }
+            });
+    });
   });
 }
 
